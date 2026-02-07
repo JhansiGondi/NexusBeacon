0530cfad6df6018f94c50557bde3014abcd9d335
Jhansi Gondi <g.jhansi1081@gmail.com>
2026-02-07 23:27:34 +0530

    Force HTTPS scheme in production to fix asset loading

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+// Force HTTPS in production
+if (app()->environment('production')) {
+    \Illuminate\Support\Facades\URL::forceScheme('https');
+}
+
+return $app;
